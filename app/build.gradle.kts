dc878d57ea7d8e12af4f35d6a56d90fa0e1853e1
Makinul Hasan <mcnasim@gmail.com>
2025-10-07 13:58:26 +0600

    feat: Update dependencies and add material icons
    - Upgraded various library versions in `gradle/libs.versions.toml`, including AGP, Kotlin, Compose BOM, Ktor, and Firebase BOM.
    - Added the `material-icons-extended` dependency to `libs.versions.toml` and implemented it in `app/build.gradle.kts`.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -78,6 +78,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.material.icons.extended)
 
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.animation)
